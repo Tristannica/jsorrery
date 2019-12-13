d027d4d5d7d2e0b96469c39779f65faeb1ba9b61
Tristan Gilford <gilford.tristan@gmail.com>
2019-12-12 20:22:50 -0800

    + orbital parameters for 21616 Guhagilford 🚀

---
+++ src/scenario/scenarios/bodies/guhagilford.js
@@ -0,0 +1,28 @@
+import { AU } from '../../../core/constants';
+
+export const guhagilford = {
+    title: '21616 Guhagilford (1999 JQ82)',
+    name: 'guhagilford',
+    mass: 3.8e4,
+    radius: 2.563,
+    color: '#F50057',
+    map: './assets/img/guhagilfordmap.jpg',
+    orbit: {
+        base: {
+            a: 2.35484790 * AU,
+            e: 0.12713336,
+            i: 6.74028159,
+            l: 151.26807617,
+            lp: 172.98973236,
+            o: 102.80456657,
+        },
+        cy: {
+            a: -0.00196176 * AU,
+            e: -0.00004397,
+            i: -0.00242939,
+            l: 428.48202785,
+            lp: 0.40805281,
+            o: 0.04240589,
+        },
+    },
+};
